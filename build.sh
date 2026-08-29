6ae1bdc9ae6967455be7a2c0e05cae4015e35f19
demianiuk-yana27 <demianiuk.yana27@gmail.com>
2026-08-29 19:15:29 +0300

    Update build script to install Chromium and dependencies
    Added commands to update package managers and install Chromium and its dependencies before installing Python requirements.

--- build.sh
+++ build.sh
@@ -1,2 +1,9 @@
 #!/usr/bin/env bash
+set -o errexit
+
+# Оновлюємо менеджери пакетів і ставимо Chromium та його залежності
+apt-get update && apt-get install -y \
+    chromium \
+    chromium-driver
+
 pip install -r requirements.txt
