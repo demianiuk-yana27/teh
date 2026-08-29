eb2c23235032f8934cad36934f633fdf15e4feda
demianiuk-yana27 <demianiuk.yana27@gmail.com>
2026-08-29 18:47:28 +0300

    Update build script to include additional packages

--- build.sh
+++ build.sh
@@ -1,4 +1,3 @@
 #!/usr/bin/env bash
-# оновлення та встановлення системного браузера та залежностей
-apt-get update && apt-get install -y chromium-browser chromium-chromedriver
+apt-get update && apt-get install -y wget curl unzip libxi6 libgconf-2-4 libnss3 chromium-browser chromium-chromedriver
 pip install -r requirements.txt
